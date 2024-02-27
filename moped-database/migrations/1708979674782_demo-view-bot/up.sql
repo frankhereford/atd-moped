94a6f07e7d30e8c5509b12dc5df378db30f62160
Frank Hereford <frankhereford@users.noreply.github.com>
2024-02-27 15:13:41 -0600

    Forgot to export the variable

--- moped-database/migrations/1708979674782_demo-view-bot/up.sql
+++ moped-database/migrations/1708979674782_demo-view-bot/up.sql
@@ -1,4 +1,4 @@
 CREATE VIEW dad_joke AS
 SELECT
     'Did you hear about the mathematician who is afraid of negative numbers?' AS setup,
-    'He will stop at nothing to avoid them!' AS punchline;
+    'He will stop at nothing to avoid them.' AS punchline;
